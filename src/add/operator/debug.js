290e1e4296b49ed19599568f59252e3e4c21e53a
KwintenP <kwinten.pisman@gmail.com>
2017-05-21 20:57:39 +0200

    Moved to separate folder.

---
+++ src/add/operator/debug.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Observable_1 = require("rxjs/Observable");
+var debug_1 = require("../../operator/debug");
+Observable_1.Observable.prototype.debug = debug_1.debug;
