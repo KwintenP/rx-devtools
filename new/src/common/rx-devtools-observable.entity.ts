fb8cf8d93f24ad121c15b97e08db281ede0aa1fb
KwintenP <kwinten.pisman@gmail.com>
2017-07-12 20:27:12 +0200

    Removed all the old folders.

--- new/src/common/rx-devtools-observable.entity.ts
+++
@@ -1,6 +0,0 @@
-export interface RxDevtoolsObservable {
-  operators: Array<{ operatorName?: string, operatorId: string, values: Array<{ percentage: number, value: any }> }>;
-  obsParents?: string[];
-  standalone: boolean;
-  name?: string;
-};
