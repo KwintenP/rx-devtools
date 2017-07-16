101f54b7dba1b34ac5b999accdb738662228cb14
KwintenP <kwinten.pisman@gmail.com>
2017-07-16 14:50:55 +0200

    Run the message receivals in ngZone to visualize them in the app.

--- devtools-page/src/app/app.component.ts
+++ devtools-page/src/app/app.component.ts
@@ -1,5 +1,4 @@
-import {Component} from '@angular/core';
-import {Http} from '@angular/http';
+import {Component, NgZone, OnInit} from '@angular/core';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/mergeMap';
 import 'rxjs/add/operator/skip';
@@ -22,46 +21,37 @@ declare const chrome;
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app works!';
   bp;
 
-  constructor(private http: Http) {
+  constructor(private zone: NgZone) {
+  }
+
+  ngOnInit() {
     var backgroundPageConnection = chrome.runtime.connect();
     this.bp = backgroundPageConnection;
+    console.log('bp', this.bp);
     console.log('connection made', backgroundPageConnection);
 
     let callback = (message, sender, sendResponse) => {
       this.title = 'message received mothafucker';
       console.log('message received', message, sender, sendResponse);
     };
-    backgroundPageConnection.onMessage.addListener(callback as any);
+    backgroundPageConnection.onMessage.addListener(this.runInZone.bind(this));
 
     backgroundPageConnection.postMessage({
       name: 'rx-devtools-page-init',
       tabId: chrome.devtools.inspectedWindow.tabId
     });
-
-    // const obs$ = Observable.interval(500).map(val => val + 1).take(2).do(console.log)
-    //     .concat(Observable.of(1, 2, 3, 4).debug("second"))
-    //     .startWith(42)
-    //     // .map(([val, val2]) => val * val2)
-    //     .filter((val) => val % 2 === 0)
-    //     .mergeMap(val => http.get("http://swapi.co/api/people/" + val))
-    //     .map(res => res.json())
-    //     .filter(_ => true)
-    //     .map(val => val.name);
-
-    // const obs$ = Observable.interval(100).debug("first").startWith(0);
-
-    // const obs$ = Observable.combineLatest(Observable.of(1, 2, 3, 4).debug(), Observable.interval(1000).skip(1).debug().take(5),
-    //   (val: number, interval: number) => val * interval)
-    //   .debug()
-    //   .mergeMap(val => http.get("http://swapi.co/api/people/" + val))
-    //   .map(res => res.json())
-    //   .map(val => val.name);
-
-    // obs$.subscribe(_ => console.log(rxDevtoolsObservables));
   }
 
+  private runInZone(message, sender, sendResponse) {
+    this.zone.run(() => this.processMesage(message, sender, sendResponse));
+  }
+
+  private processMesage(message, sender, sendResponse) {
+    this.title = 'message received mothafucker';
+    console.log('message received', message, sender, sendResponse);
+  };
 }
