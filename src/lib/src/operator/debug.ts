8f585ae7e6fd63b0c34e193c363053345bd1b662
KwintenP <kwinten.pisman@gmail.com>
2017-07-22 11:21:37 +0200

    Fixed the debug operator for combination operators.

--- src/lib/src/operator/debug.ts
+++ src/lib/src/operator/debug.ts
@@ -1,16 +1,20 @@
-import {Observable} from "rxjs/Observable";
-import {Operator} from "rxjs/Operator";
-import {Subscriber} from "rxjs/Subscriber";
-import { v4 as uuid } from 'uuid';
+import {Observable} from 'rxjs/Observable';
+import {Operator} from 'rxjs/Operator';
+import {Subscriber} from 'rxjs/Subscriber';
+import {v4 as uuid} from 'uuid';
 
 export interface DebugSignature<T> {
   (name?: string): Observable<T>;
 }
 
-export function debug(name: string): Observable {
+export function debug<T>(this: Observable<T>, name: string): Observable<T> {
   // Assign an id to the current observable being lifted. This way we can identify
   // which observable should be debugged.
-  (this as any).__rx_observable_dev_tools_id = uuid();
+  // If the observable already holds an id (which can happen with combination operators)
+  // don't reassign a new one since it will screw up the results
+  if (!(this as any).__rx_observable_dev_tools_id) {
+    (this as any).__rx_observable_dev_tools_id = uuid();
+  }
   return this.lift(new DebugOperator(name));
 }
 
